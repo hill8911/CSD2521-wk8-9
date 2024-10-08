93b070fdea578bab7216a8fb63d93d09bc0669f3
hill8911 <hilll8911@clarkstate.edu>
2024-10-08 13:09:21 -0400

    Update HelloTest1.java
    I added an extra line of comments on line 3 to signify the update and modified the comments on line 9.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/8/2024 by LH
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request!
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
